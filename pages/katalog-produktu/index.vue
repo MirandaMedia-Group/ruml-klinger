73c6cc21827f9d6f46b8f1de04cef6cc5f9b4f10
mrstikm <info@mirandamedia.cz>
2023-05-30 14:58:23 +0200

    update root category to not display products

--- pages/katalog-produktu/index.vue
+++ pages/katalog-produktu/index.vue
@@ -12,7 +12,7 @@
 					</div>
 				</div>
 				<SubcategoriesList />
-				<div
+				<!-- <div
 					id="products"
 					ref="productsAnchor">
 					<div v-if="pending">
@@ -35,7 +35,7 @@
 						@click.prevent="handleNextPage">
 						<span class="arrow"></span>
 					</button>
-				</div>
+				</div> -->
 			</template>
 			<template #sidebar>
 				<CategorySidebar />
@@ -48,57 +48,57 @@
 	definePageMeta({
 		layout: false,
 	})
-	const variables = ref({
-		first: 15,
-		last: null,
-		after: null,
-		before: null,
-	})
-	const handleNextPage = () => {
-		setTimeout(() => productsAnchor.value.scrollIntoView(), 10)
-		variables.value.after = allProductsData.value.products.pageInfo.endCursor
-		variables.value.first = 15
-		variables.value.before = null
-		variables.value.last = null
-		refresh()
-	}
-	const handlePrevPage = () => {
-		setTimeout(() => productsAnchor.value.scrollIntoView(), 10)
-		variables.value.before = allProductsData.value.products.pageInfo.startCursor
-		variables.value.last = 15
-		variables.value.first = null
-		variables.value.after = null
-		refresh()
-	}
+	// const variables = ref({
+	// 	first: 15,
+	// 	last: null,
+	// 	after: null,
+	// 	before: null,
+	// })
+	// const handleNextPage = () => {
+	// 	setTimeout(() => productsAnchor.value.scrollIntoView(), 10)
+	// 	variables.value.after = allProductsData.value.products.pageInfo.endCursor
+	// 	variables.value.first = 15
+	// 	variables.value.before = null
+	// 	variables.value.last = null
+	// 	refresh()
+	// }
+	// const handlePrevPage = () => {
+	// 	setTimeout(() => productsAnchor.value.scrollIntoView(), 10)
+	// 	variables.value.before = allProductsData.value.products.pageInfo.startCursor
+	// 	variables.value.last = 15
+	// 	variables.value.first = null
+	// 	variables.value.after = null
+	// 	refresh()
+	// }
 	const productsAnchor = ref(null)
 	const screenWidth = useState('screenWidth')
-	const allProductsQuery = gql`
-		query getRootCategory($first: Int, $last: Int, $after: String, $before: String) {
-			products(first: $first, last: $last, after: $after, before: $before, where: { orderby: { field: MENU_ORDER, order: DESC } }) {
-				nodes {
-					slug
-					title
-					productAcf {
-						shortDescription
-						gallery {
-							sourceUrl
-							mediaDetails {
-								width
-								height
-							}
-						}
-					}
-				}
-				pageInfo {
-					endCursor
-					hasNextPage
-					hasPreviousPage
-					startCursor
-				}
-			}
-		}
-	`
-	const { data: allProductsData, refresh, pending } = await useAsyncQuery(allProductsQuery, variables.value)
+	// const allProductsQuery = gql`
+	// 	query getRootCategory($first: Int, $last: Int, $after: String, $before: String) {
+	// 		products(first: $first, last: $last, after: $after, before: $before, where: { orderby: { field: MENU_ORDER, order: DESC } }) {
+	// 			nodes {
+	// 				slug
+	// 				title
+	// 				productAcf {
+	// 					shortDescription
+	// 					gallery {
+	// 						sourceUrl
+	// 						mediaDetails {
+	// 							width
+	// 							height
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 			pageInfo {
+	// 				endCursor
+	// 				hasNextPage
+	// 				hasPreviousPage
+	// 				startCursor
+	// 			}
+	// 		}
+	// 	}
+	// `
+	// const { data: allProductsData, refresh, pending } = await useAsyncQuery(allProductsQuery, variables.value)
 </script>
 <style lang="scss">
 	#products {
