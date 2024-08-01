481aa01be2b5d6b29270b8bf14b42af1ae41d9f1
mrstikm-miranda <77618318+MirandaMediaGroup@users.noreply.github.com>
2024-08-01 18:20:24 +0200

    packages update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,18 +58,6 @@ export default defineNuxtConfig({
 			siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://www.ruml-klinger.cz',
 		},
 	},
-
-	// gtm: {
-	// 	id: 'GTM-PVPZKVF',
-	// 	defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
-	// 	compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`
-	// 	// nonce: '2726c7f26c', // Will add `nonce` to the script tag
-	// 	enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
-	// 	debug: true, // Whether or not display console logs debugs (optional)
-	// 	loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
-	// 	enableRouterSync: true, // Pass the router instance of your app to automatically sync with router (optional)
-	// 	trackOnNextTick: false, // Whether or not call trackView in Vue.nextTick
-	// },
 	gtag: {
 		id: 'GTM-PVPZKVF',
 		initCommands: [
@@ -170,6 +158,4 @@ export default defineNuxtConfig({
 		defaultLocale: 'cs',
 		vueI18n: './i18n.config.ts',
 	},
-
-	compatibilityDate: '2024-08-01',
 })
